f7b2a541a47e942023b7e5a3d78ca0db268d8756
Rodrigo Freitas <rodrigo.c.freitas@outlook.com>
2020-07-14 23:43:32 -0300

    Update Banco.java
    a-05 parcial

--- CursoJava-a05/src/cursojava/a05/Banco.java
+++ CursoJava-a05/src/cursojava/a05/Banco.java
@@ -7,8 +7,81 @@ package cursojava.a05;
 
 /**
  *
- * @author rodri
+ * @author Rodrigo Freitas
  */
 public class Banco {
+    //Atributos
+    public int numConta;
+    protected String tipo;
+    private String dono;
+    private float saldo;
+    private boolean status;
+
+    public Banco() {
+        this.saldo = 0;
+        this.status = false;
+    }
+    
+    public void abrirConta() {
+        this.setStatus(true);
+        if(this.getTipo() == "cc") {
+            this.setSaldo(50f);
+        } else if(this.getTipo() == "cp") {
+            this.setSaldo(150f);
+        }
+        
+    }
+    
+    public void fecharConta() {
+        if(this.getSaldo() != 0) {
+            System.out.println("Você possui débitos ou créditos na conta. Verifique sua conta antes de fechar");
+        } else {
+            this.setStatus(false);
+            System.out.println("Conta fechada com sucesso! Volte sempre!");   
+        }
+    }
+
+    public int getNumConta() {
+        return numConta;
+    }
+
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDono() {
+        return dono;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+    
+    
+    
     
 }
