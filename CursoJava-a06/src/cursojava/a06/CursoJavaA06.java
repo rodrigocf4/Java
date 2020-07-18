ad962b39d138d64b8e8bb9802a2f7ed134b781eb
Rodrigo Freitas <rodrigo.c.freitas@outlook.com>
2020-07-17 23:41:34 -0300

    Aula06-Encapsulamento
    Exercício Controle Remoto com encapsulamento - getters e setters também privados.

---
+++ CursoJava-a06/src/cursojava/a06/CursoJavaA06.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cursojava.a06;
+
+/**
+ *
+ * @author Rodrigo Freitas
+ */
+public class CursoJavaA06 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto(); {
+        //c.ligar();
+        c.maisVolume();
+        c.play();
+        c.abrirMenu();
+        c.fecharMenu();
+    }
+    }
+    
+}
